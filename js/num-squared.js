fdfaf0029c5c2aa9b5b0f8be5217e7fc84fe40ce
rashid <abdulrashid.hayato@edu.sait.ca>
2023-03-01 18:28:43 -0700

    adding in repo

---
+++ js/num-squared.js
@@ -0,0 +1,23 @@
+// Step 1: Declare function
+const num = 4;
+const square = function( num ) {
+  
+  const numSquared = num * num;
+  
+  console.log(`${num} squared is ${numSquared}.`);
+};
+// Refactor the following code into a function so that it:
+// 1. accepts a the num variable as a parameter;
+
+// 2. squares the num and returns it as a complete sentence;
+
+square ( num )
+// 3. logs the returned squared num to the console, from outside the function.
+
+
+// This console log should log the returned value from outside the function
+
+
+// console.log(`${num} squared is ${numSquared}.`);
+
+
